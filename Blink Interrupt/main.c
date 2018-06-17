708b3d7cd0a3111de0c032b20fdc391977b05550
sunshine2050 <ahmedsamy10.28@gmail.com>
2018-06-18 00:05:15 +0200

    Blink LED when interrupted with a push Button using ISR

---
+++ Blink Interrupt/main.c
@@ -0,0 +1,29 @@
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int main(void)
+{
+	DDRA |= (1<<PA0);
+	DDRB |= (1<<PB0);
+	DDRD &= ~(1<<PD2);
+	PORTD |= (1<<PD2);
+	
+	MCUCR |= (1<<ISC01);
+	
+	GICR |= (1<<INT0);
+	
+	sei();
+	
+	while(1)
+	{
+		PORTA ^= (1<<PA0);
+		_delay_ms(500);
+	}
+
+}
+
+ISR(INT0_vect){
+		PORTB ^= (1<<PB0);
+}
